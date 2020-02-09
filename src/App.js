edcd3cd49d32c34318fe0ba8c4d5ac1ad96465c2
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2020-02-08 23:08:13 -0300

    adicionando formatação aos componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import './App.css';
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
 import Routes from './Routes';
 
 function App() {
-  return (
-    <div>
-      <div className="nav-bar">
-        <NavLink className="link" activeClassName="active" exact to="/">
-          Inicio
-        </NavLink>
-        <NavLink className="link" activeClassName="active" to="/favs">
-          Favoritos
-        </NavLink>
-        <NavLink className="link" activeClassName="active" to="/login">
-          Login
-        </NavLink>
-      </div>
-      <Routes />
-    </div>
-  );
+	return (
+		<div>
+			<div className="nav-bar">
+				<NavLink className="link" activeClassName="active" exact to="/">
+					Inicio
+				</NavLink>
+				<NavLink className="link" activeClassName="active" to="/favs">
+					Favoritos
+				</NavLink>
+				<NavLink className="link" activeClassName="active" to="/login">
+					Login
+				</NavLink>
+			</div>
+			<Routes />
+		</div>
+	);
 }
 
 export default App;
